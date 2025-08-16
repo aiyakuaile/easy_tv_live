4c07efb2fe14e1e4f1e03a7523ed52a0cf2e7f59
aiyakuaile <amos93workspace@gmail.com>
2025-08-16 23:47:41 +0800

    更新 update_temp.py

--- update_temp.py
+++ update_temp.py
@@ -2,8 +2,8 @@
 import requests
 
 # 目标地址
-# TARGET_URL = "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/ipv6/result.m3u"
-TARGET_URL = "https://iptv-org.github.io/iptv/countries/cn.m3u"
+TARGET_URL = "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/ipv6/result.m3u"
+# TARGET_URL = "https://iptv-org.github.io/iptv/countries/cn.m3u"
 # 要写入的本地文件路径（项目中的temp文件）
 LOCAL_FILE = "temp"
 
